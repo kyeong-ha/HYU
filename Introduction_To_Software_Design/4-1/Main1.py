d0224b8b918fed6a6bc603deac74a6cf708d22da
KyeongHa <rudgk030@hanyang.ac.kr>
2023-02-17 23:14:13 +0900

    2018-03-29

---
+++ Introduction_To_Software_Design/4-1/Main1.py
@@ -0,0 +1,15 @@
+import func1
+import func2
+import func3
+
+while True:
+    num = input()
+    height = input()
+    if num == 1 :
+        print func1.func1(height)
+
+    if num == 2 :
+        print func2.func2(height)
+
+    if num == 3 :
+        print func3.func3(height)
