1c8cfb1cde20a0223da52474eea12dc6c626f70f
KyeongHa <rudgk030@hanyang.ac.kr>
2023-04-18 00:38:22 +0900

    2023-04-18

---
+++ Computer_Graphics/assignment-4/LabAssignment4-2.py
@@ -0,0 +1,59 @@
+import glfw
+from OpenGL.GL import *
+import numpy as np
+
+def drawUnitCube():
+    glBegin(GL_QUADS)
+    glVertex3f( 0.5, 0.5,-0.5)
+    glVertex3f(-0.5, 0.5,-0.5)
+    glVertex3f(-0.5, 0.5, 0.5)
+    glVertex3f( 0.5, 0.5, 0.5) 
+                             
+    glVertex3f( 0.5,-0.5, 0.5)
+    glVertex3f(-0.5,-0.5, 0.5)
+    glVertex3f(-0.5,-0.5,-0.5)
+    glVertex3f( 0.5,-0.5,-0.5) 
+                             
+    glVertex3f( 0.5, 0.5, 0.5)
+    glVertex3f(-0.5, 0.5, 0.5)
+    glVertex3f(-0.5,-0.5, 0.5)
+    glVertex3f( 0.5,-0.5, 0.5)
+                             
+    glVertex3f( 0.5,-0.5,-0.5)
+    glVertex3f(-0.5,-0.5,-0.5)
+    glVertex3f(-0.5, 0.5,-0.5)
+    glVertex3f( 0.5, 0.5,-0.5)
+ 
+    glVertex3f(-0.5, 0.5, 0.5) 
+    glVertex3f(-0.5, 0.5,-0.5)
+    glVertex3f(-0.5,-0.5,-0.5) 
+    glVertex3f(-0.5,-0.5, 0.5) 
+                             
+    glVertex3f( 0.5, 0.5,-0.5) 
+    glVertex3f( 0.5, 0.5, 0.5)
+    glVertex3f( 0.5,-0.5, 0.5)
+    glVertex3f( 0.5,-0.5,-0.5)
+    glEnd()
+
+def drawCubeArray():
+    for i in range(5):
+        for j in range(5):
+            for k in range(5):
+                glPushMatrix()
+                glTranslatef(i,j,-k-1)
+                glScalef(.5,.5,.5)
+                drawUnitCube()
+                glPopMatrix()
+
+def drawFrame():
+    glBegin(GL_LINES)
+    glColor3ub(255, 0, 0)
+    glVertex3fv(np.array([0.,0.,0.]))
+    glVertex3fv(np.array([1.,0.,0.]))
+    glColor3ub(0, 255, 0)
+    glVertex3fv(np.array([0.,0.,0.]))
+    glVertex3fv(np.array([0.,1.,0.]))
+    glColor3ub(0, 0, 255)
+    glVertex3fv(np.array([0.,0.,0]))
+    glVertex3fv(np.array([0.,0.,1.]))
+    glEnd()
